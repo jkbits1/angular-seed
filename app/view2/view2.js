c66651b6a3d211ab8893fd29fa2a06ec41497392
jkbits1 <jkbits1@users.noreply.github.com>
2015-01-11 18:27:10 +0000

    added new view that shares code using a service

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,15 +1,126 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+var view2Module = angular.module('myApp.view2', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
     templateUrl: 'view2/view2.html',
     controller: 'View2Ctrl'
   });
-}])
+}]);
 
-.controller('View2Ctrl', ['$scope', '$http', function($scope, $http) {
+view2Module.factory('filenameService', function () {
+
+  //return function (){
+
+  var t1 = "";
+
+
+  return {
+
+    // NOTE: although both methods work, not sure whether to use this.fn or pass in filenameService as an arg and use filenameService.fn
+    setUpScope: function($scope) {
+
+      $scope.getDateAsString = this.getDateAsString;
+
+      $scope.getProgramName = this.getProgramName($scope);
+
+      $scope.getFirstEpisodeDate = this.getFirstEpisodeDate
+        //($scope)
+      ;
+      $scope.getLastEpisodeDate = this.getLastEpisodeDate($scope);
+
+      $scope.getEpisodeCount = this.getEpisodeCount($scope);
+    },
+    getFileName: function (x) {
+      return "123";
+    },
+    getDateAsString: function (date) {
+      var fileDate = new Date(date);
+      return fileDate.toDateString();
+    },
+    //getProgramName: function () {
+    //
+    //  if ($scope.files === undefined) {
+    //    return "";
+    //  }
+    //
+    //  return $scope.files[0].fileName;
+    //},
+    getProgramName: function ($scope) {
+
+      return function() {
+
+        if ($scope.files === undefined) {
+          return "";
+        }
+
+        return $scope.files[0].fileName;
+      };
+    },
+
+    // NOTE: both patterns below work equally well (for all service fns), i) using a closure to allow use of scope, and ii) simply using this, relying on that being scope. I prefer the closure as more readable, possibly future-proof but is perhaps slightly less efficient.
+    //getFirstEpisodeDate: function($scope) {
+    //
+    //  return function() {
+    //
+    //    if ($scope.files === undefined) {
+    //
+    //      return "";
+    //    }
+    //
+    //    return new Date($scope.files[0].date).toDateString();
+    //  }
+    //},
+    getFirstEpisodeDate: function() {
+
+        //if ($scope.files === undefined) {
+        if (this.files === undefined) {
+
+          return "";
+        }
+
+        //return new Date($scope.files[0].date).toDateString();
+        return new Date(this.files[0].date).toDateString();
+    },
+
+    getLastEpisodeDate: function($scope) {
+
+      return function() {
+
+        if ($scope.files === undefined) {
+
+          return "";
+        }
+
+        var lastEpisodeIndex = $scope.files.length - 1;
+
+        return new Date($scope.files[lastEpisodeIndex].date).toDateString();
+      }
+    },
+
+    getEpisodeCount: function($scope) {
+
+      return function() {
+        if ($scope.files === undefined) {
+
+          return 0;
+        }
+
+        return $scope.files.length;
+      }
+    }
+  };
+
+  //var t1 = "";
+  //return function(x){
+  //    return "123";
+  //}
+  //}
+
+});
+
+view2Module.controller('View2Ctrl', ['$scope', '$http', 'filenameService', function($scope, $http, filenameService) {
 
     $http.get('http://localhost:3030')
       .success(function (data, status, headers, config) {
@@ -63,56 +174,7 @@ angular.module('myApp.view2', ['ngRoute'])
 
     });
 
-    $scope.getDateAsString = function(date) {
-
-      var fileDate = new Date(date);
-
-      return fileDate.toDateString();
-    };
-
-    $scope.getProgramName = function() {
-
-      if ($scope.files === undefined) {
-
-        return "";
-      }
-
-      return $scope.files[0].fileName;
-    };
-
-    $scope.getFirstEpisodeDate = function() {
-
-      if ($scope.files === undefined) {
-
-        return "";
-      }
-
-      return new Date($scope.files[0].date).toDateString();
-    };
-
-    $scope.getLastEpisodeDate = function() {
-
-      if ($scope.files === undefined) {
-
-        return "";
-      }
-
-      var lastEpisodeIndex = $scope.files.length - 1;
-
-      return new Date($scope.files[lastEpisodeIndex].date).toDateString();
-    };
-
-    $scope.getEpisodeCount = function() {
-
-      if ($scope.files === undefined) {
-
-        return 0;
-      }
-
-      return $scope.files.length;
-    };
-
-
+    filenameService.setUpScope($scope);
   }]);
 
 function fileParser(file) {
---
+++ app/view3/view3.js
@@ -0,0 +1,139 @@
+/**
+ * Created by jk on 08/01/15.
+ */
+
+'use strict';
+
+var view3Module =
+
+angular.module('myApp.view3', ['ngRoute'])
+
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/view3', {
+      templateUrl: 'view3/view3.html',
+      controller: 'View3Ctrl'
+    });
+  }]);
+
+// revised to use factory call - maybe Angular upgrade has renamed calls
+//app.service("filenameService", function () {
+//
+//  var t1 = "";
+//  return {
+//    getFileName: function(x){
+//      return "123";
+//    }
+//  }
+//});
+
+view3Module.controller('View3Ctrl', ['$scope', '$http', 'filenameService', function($scope, $http, filenameService) {
+
+    $http.get('http://localhost:3030')
+      .success(function (data, status, headers, config) {
+        $scope.files = data.files;
+
+        //var t1a = filenameService.getFileName("1");
+        var getFileName = filenameService.getFileName;
+        //var getFileName = filenameService;
+        var t1a = getFileName("1");
+
+        var monthYears = [];
+
+        $scope.fileDates = $scope.files.map(function (file) {
+
+          var dateWithoutWeekday = $scope.getDateAsString(file.date);
+          var dateWithoutWeekdayLetters = dateWithoutWeekday.split('');
+
+          var month = dateWithoutWeekdayLetters.splice(4, 3).join('');
+          // remove space
+          //dateWithoutWeekdayLetters.splice(0, 1);
+          var day = dateWithoutWeekdayLetters.splice(5, 2).join('');
+          var year = dateWithoutWeekdayLetters.splice(6, 4).join('');
+
+          var monthInfo = "";
+
+          if (monthYears[year + month] === undefined) {
+
+            monthYears[year + month] = 1;
+
+            //monthInfo = year + "&nbsp;&nbsp;&nbsp;&nbsp;" + month;
+            monthInfo = year + "  " + month;
+          }
+          else {
+
+            monthYears[year + month] += 1;
+            //monthInfo = "&nbsp;";
+            monthInfo = "";
+            month = "";
+            year = "";
+          }
+
+          return {
+            monthYear: monthInfo,
+            year: year,
+            month: month,
+            day: day
+          }
+        });
+
+        //var parser = new fileParser($scope.files[0].fileName);
+
+        $scope.fileName = $scope.getProgramName();
+        //$scope.fileDate = $scope.files[0].date;
+
+      }).error(function(data, status, headers, config) {
+
+      });
+
+    filenameService.setUpScope($scope);
+
+    //$scope.getDateAsString = function(date) {
+    //
+    //  var fileDate = new Date(date);
+    //
+    //  return fileDate.toDateString();
+    //};
+    //
+    //$scope.getProgramName = function() {
+    //
+    //  if ($scope.files === undefined) {
+    //
+    //    return "";
+    //  }
+    //
+    //  return $scope.files[0].fileName;
+    //};
+    //
+    //$scope.getFirstEpisodeDate = function() {
+    //
+    //  if ($scope.files === undefined) {
+    //
+    //    return "";
+    //  }
+    //
+    //  return new Date($scope.files[0].date).toDateString();
+    //};
+    //
+    //$scope.getLastEpisodeDate = function() {
+    //
+    //  if ($scope.files === undefined) {
+    //
+    //    return "";
+    //  }
+    //
+    //  var lastEpisodeIndex = $scope.files.length - 1;
+    //
+    //  return new Date($scope.files[lastEpisodeIndex].date).toDateString();
+    //};
+    //
+    //$scope.getEpisodeCount = function() {
+    //
+    //  if ($scope.files === undefined) {
+    //
+    //    return 0;
+    //  }
+    //
+    //  return $scope.files.length;
+    //};
+  }]);
+
