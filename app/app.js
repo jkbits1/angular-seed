95f197afe224c11d226f3077eda49ced32c66fa8
jkbits1 <jkbits1@users.noreply.github.com>
2015-01-31 04:18:39 +0000

    renamed view3 to viewBroadcasts

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ var myApp = angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.view3',
+  'myApp.viewBroadcasts',
   'myApp.viewChooseProgramme',
   'myApp.version'
 ]);
